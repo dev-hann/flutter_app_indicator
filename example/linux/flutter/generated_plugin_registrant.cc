a5362205c96c4f7ca9631fd2f6724861439da638
dev-hann <developer.hann@gmail.com>
2022-05-02 13:44:43 +0800

    first commit

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_app_indicator/flutter_app_indicator_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_app_indicator_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterAppIndicatorPlugin");
+  flutter_app_indicator_plugin_register_with_registrar(flutter_app_indicator_registrar);
+}
